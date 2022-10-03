cd6fef77588bbe542c1d90282458f6a7ac547faf
saira512dev <512saira@gmail.com>
2022-10-03 18:34:48 +0530

    Typo

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,7 @@ export default function Interests(){
     return (
         <div className="about">
             <h3>Interests</h3>
-            <p>Bookaholic, love trying out new recipes. Deeply interested in learning new languages. Never get tired of neuroscience.
+            <p>Book-o-holic, love trying out new recipes. Deeply interested in learning new languages. Never get tired of neuroscience.
             </p>
         </div>
     )
